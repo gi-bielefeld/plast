d9fa8841823c52c62badc3c455652776c0aacfca
tischulz1 <tischulz@cebitec.uni-bielefeld.de>
2026-09-03 11:43:05 +0200

    fix: Modified result statistics script to prevent workflow crash

--- comparison/scripts/showPLASTresStats.py
+++ comparison/scripts/showPLASTresStats.py
@@ -23,8 +23,12 @@ for l in arguments.R:
 		lengths.append(int(elems[1].split(' ')[1]))
 		evals.append(float(elems[2].split(' ')[1]))
 
-#Output descriptive stats
-print("Number of found results:", nbRes)
-print(f"Results scores: max: {max(scores)} min: {min(scores)} avg.: {mean(scores)} std.: {std(scores)}")
-print(f"Results lengths: max: {max(lengths)} min: {min(lengths)} avg.: {mean(lengths)} std.: {std(lengths)}")
-print(f"Results e-values: max: {max(evals)} min: {min(evals)} avg.: {mean(evals)} std.: {std(evals)}")
+#Check if any results were found
+if nbRes > 0:
+	#Output descriptive stats
+	print("Number of found results:", nbRes)
+	print(f"Results scores: max: {max(scores)} min: {min(scores)} avg.: {mean(scores)} std.: {std(scores)}")
+	print(f"Results lengths: max: {max(lengths)} min: {min(lengths)} avg.: {mean(lengths)} std.: {std(lengths)}")
+	print(f"Results e-values: max: {max(evals)} min: {min(evals)} avg.: {mean(evals)} std.: {std(evals)}")
+else:
+	print("---No significant alignments found---")
